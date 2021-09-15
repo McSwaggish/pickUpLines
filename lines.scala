d0c8e931dc8f042ec887010aaf6ca0e127144c97
Erik Nicander <toomanynamesreally@gmail.com>
2021-09-15 13:05:26 +0200

    jag kommer leva för evigt

---
+++ lines.scala
@@ -0,0 +1,27 @@
+import scala.io.AnsiColor._
+/*
+   ___   ___   ___  ___ ___  ___ __ __ __  __ __  __  ___  ___          
+  //    // \\  ||\\//|| ||\\//|| || || ||\ || || (( \ ||\\//||          
+ ((    ((   )) || \/ || || \/ || || || ||\\|| ||  \\  || \/ ||          
+  \\__  \\_//  ||    || ||    || \\_// || \|| || \_)) ||    ||          
+                                                                        
+  __  __  __  ___  __    __       ____  ____   ____ __ __  ___  __ __   
+ (( \ ||  || // \\ ||    ||       || \\ || \\ ||    || || // \\ || ||   
+  \\  ||==|| ||=|| ||    ||       ||_// ||_// ||==  \\ // ||=|| || ||   
+ \_)) ||  || || || ||__| ||__|    ||    || \\ ||___  \V/  || || || ||__|
+                                                                        
+*/
+@main
+def main: Unit =
+    val x = /*(math.random * 10).toInt*/ 7
+    
+    if x == 2 then println("hård dubstep ;)")
+    else if x == 3 then println("tjenare smulan står du här o kexar ;)")
+    else if x == 4 then println("boy ur perfect i see it in ur eyes ;)")
+    else if x == 5 then println("i wanna touch it i never have enough ;)")
+    else if x == 6 then println("damn shawty what that phone number do ;)")
+    else if x == 6 then println("were smart enough to know that no one get out alive, hold on to someone close friend, cause youre never too young, never too young to die, remeber them ghosts before you drink to their souls tonight dont forget what they told you youre never too young youre never too young to die, *LOGIC RAPS* ;)")
+    else println(s"""${RED} __    __  ___  ____ ____ __ __ ____       ___  __ ____  __   
+                    |${YELLOW} ||    || // \\\\   //   // || || || \\\\     // \\\\ || || \\\\ ||   
+                    |${GREEN} \\\\ /\\ // ||=||  //   //  || || ||_//    (( ___ || ||_// ||   
+                    |${BLUE}  \\V/\\V/  || || //__ //__ \\\\_// ||        \\\\_|| || || \\\\ ||__| ;)""".stripMargin)
